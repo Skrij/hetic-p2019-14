db06d0471933fafe751f7884f3227950cc5e5273
LaurenceRouge <laurence.rouge@hetic.net>
2016-10-24 23:34:48 +0200

    scss + gulp
    Gulp en process

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+sass = require('gulp-sass');
+sync = require('browser-sync').create();
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+var processors = [
+autoprefixer
+];
+
+gulp.task('sass', function() {
+return gulp.src('src/styles/scss/*.scss')
+.pipe(sass().on('error', sass.logError))
+.pipe(postcss(processors))
+.pipe(gulp.dest('src/styles'))
+});
+
+gulp.task('default', function () {
+  gulp.watch('src/css/scss/*.scss', ['sass']);
+});
