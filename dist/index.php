ab4fc5dea024a7c6895703399429ab84b5a04ac9
Cossid Alexandra (Hetic) <Alexandra@MacBPro-Alex-Cssd.local>
2016-11-03 17:53:14 +0100

    [index] : avancement avec réorganisation fichiers

--- dist/index.php
+++ dist/index.php
@@ -1,22 +1,28 @@
 <?php include './partials/header.php';?>
 
-<div class="home">
-	<div class="intro">
-		<div class="intro__container">
-			<p class="intro__datelimit">J-242</p>
-			<p class="intro__title">Aidez-nous à sauver l'Arctique</p>
-			<div class="intro__action">
-				<span class="intro__action__sign">
-					<img src="" alt="">
-					<p class="action__text"><a href="main-page.php">Signez</a></p>
-				</span>
-				<span class="intro__action__share">
-					<img src="" alt="">
-					<p class="action__text">Partagez</p>
-				</span>
-			</div>
-		</div>
-	</div>	
-</div>
+<div class="intro">
+    
+    <div class="intro__content">
+        <p class="intro__datelimit">J-242</p>
+        <p class="intro__title">Aidez-nous à <br> sauver l'Arctique</p>
+    </div> <!-- .intro__container -->
+
+     <div class="intro__action">
+        <div class="action__button action__button--sign">
+            <img src="" >
+            <p class="action__text">Signez</p>
+        </div>
+        <div class="action__button action__button--share">
+            <img src="" >
+            <p class="action__text">Partagez</p>
+        </div>
+    </div> <!-- .intro__action -->
+
+    <div class="intro__more">
+    	<a href="main-page.php"> En savoir plus </a>
+    	<img src="" >
+    </div>
+
+</div> <!-- .intro -->
 
 <?php include './partials/footer.php';?>
