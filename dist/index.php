fdc22f482788f03732518b726762dd840dfc04e3
Cossid Alexandra (Hetic) <Alexandra@MacBPro-Alex-Cssd.local>
2016-11-03 10:19:42 +0100

    [Debug] : master

--- dist/index.php
+++ dist/index.php
@@ -1,11 +1,22 @@
 <?php include './partials/header.php';?>
-
-<nav>
-  
-</nav>
-
-<div id="content">
-
+<div class="home">
+	<div class="intro">
+		<div class="content-home">
+			<p class="intro__datelimit">J-242</p>
+			<p class="intro__title">Aidez-nous à sauver l'Arctique</p>
+			<div class="intro__action">
+				<span class="intro__action__sign">
+					<img src="" alt="">
+					<p class="action__sign__text">Signez</p>
+				</span>
+				<span class="intro__action__share">
+					<img src="" alt="">
+					<p class="action__share__text">Partagez</p>
+				</span>
+			</div>
+		</div>
+	</div>	
+	
 </div>
 
 <?php include './partials/footer.php';?>
