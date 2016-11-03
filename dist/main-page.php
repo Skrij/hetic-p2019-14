3643edea234b8576e773c2391db21dbbf71594ca
Cossid Alexandra (Hetic) <Alexandra@MacBPro-Alex-Cssd.local>
2016-11-03 11:54:12 +0100

    [main-page] : squelette
    Mise en place du squelette de la page

--- dist/main-page.php
+++ dist/main-page.php
@@ -1,3 +1,111 @@
 <?php include './partials/header.php';?>
 
+	<div class="join_container">
+		<div class="join_content join_sign"> Signez </div>
+		<div class="join_content join_share"> Partager </div>
+	</div>
+
+	<div class="evolution_container">
+		<div class="evolution_svg-container">
+			<svg class="svg_content svg_content-1"> <!-- artique --> </svg>
+			<svg class="svg_content svg_content-2"> <!-- revendication de la russie --> </svg>
+			<svg class="svg_content svg_content-3"> <!-- zone protégé par greenpeace --> </svg>
+		</div>
+
+		<div class="animals_container">
+			<h2 class="container-title animals_header"> Une faune unique à préserver </h2>
+			<div class="animals_liste">
+				<div class="animal_content animal-1">
+					<div class="animal-img">
+						<img src="#" alt="animal-1">
+					</div>
+					<div class="animal-svg"></div>
+				</div>
+				<div class="animal_content animal-2">
+					<div class="animal-img">
+						<img src="#" alt="animal-2">
+					</div>
+					<div class="animal-svg"></div>
+				</div>
+				<div class="animal_content animal-3">
+					<div class="animal-img">
+						<img src="#" alt="animal-3">
+					</div>
+					<div class="animal-svg"></div>
+				</div>
+				<div class="animal_content animal-4">
+					<div class="animal-img">
+						<img src="#" alt="animal-4">
+					</div>
+					<div class="animal-svg"></div>
+				</div>
+				<div class="animal_content animal-5">
+					<div class="animal-img">
+						<img src="#" alt="animal-5">
+					</div>
+					<div class="animal-svg"></div>
+				</div>
+				<div class="animal_content animal-6">
+					<div class="animal-img">
+						<img src="#" alt="animal-6">
+					</div>
+					<div class="animal-svg"></div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+	<div class="graph_container">
+		<h2 class="container-title graph_header"> Une banquise à protéger </h2>	
+		<img src="#" alt="graph-perte-75%">
+		<p class="graph_content"> La banquise joue un rôle critique dans la régulation du climat. Mais en raison d changements climatiques, 75% de la surface de la banquise à disparu en 30 ans. </p>
+	</div>
+
+	<div class="menace_container">
+		<h2 class="container-title menace_header"> Des menaces à arreter </h2>
+		<div class="menace_liste">
+			<div class="menace_content">
+				<div class="menace_img menace-1">
+					<img src="" alt="Shell">
+				</div>
+				<div class="menace-title"> Shell </div>
+			</div>
+			<div class="menace_content">
+				<div class="menace_img menace-2">
+					<img src="" alt="Shell">
+				</div>
+				<div class="menace-title"> Statoil </div>
+			</div>
+			<div class="menace_content">
+				<div class="menace_img menace-3">
+					<img src="" alt="Shell">
+				</div>
+				<div class="menace-title"> Rosneft </div>
+			</div>
+			<div class="menace_content">
+				<div class="menace_img menace-4">
+					<img src="" alt="Shell">
+				</div>
+				<div class="menace-title"> Gazprom </div>
+			</div>
+		</div>	
+		<p class="menace_text-container"> Avec la fonte des glaces, les industriels envisagent d'exploiter le pétrole, les métaux précieux ou les ressources halieutique; et d'emprunter les voies de navigation plus rapide au nord. Les risques sont multiples : marées noires, contamination, polution acoustisque sous-marine,arrivée d'esp§ces invasive, surpêche destruction environnementale... </p>
+	</div>
+
+	<div class="join-us_container">
+		<div class="join-us_img">
+			<img src="" alt="join-us">
+		</div>
+		<h3 class="join-us_title"> En préservant l'Artique, c'est nous-même que nous préservons</h3>
+		<div class="join-us_button"> Rejoingez-nous</div>
+		<div class="join-us_social">
+			<a href="javascript();" class="facebook"> facebook </a>
+			<a href="javascript();" class="twitter"> twitter </a>
+			<a href="javascript();" class="google+"> google+ </a>
+		</div>
+	</div>
+
+	
+
 <?php include './partials/footer.php';?>
