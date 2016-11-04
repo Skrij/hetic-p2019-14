218ca5f4a7fdd6039689311b0b71955240f5a85d
Cossid Alexandra (Hetic) <Alexandra@MacBPro-Alex-Cssd.local>
2016-11-04 12:25:04 +0100

    [home] : avancement maquette + [variable.scss] : création

--- app/index.php
+++ app/index.php
@@ -1,28 +1,29 @@
 <?php include './partials/header.php';?>
+<div class="home">
+    <div class="intro">
+        
+        <div class="intro__content">
+            <p class="intro__datelimit">J-242</p>
+            <p class="intro__title">Aidez-nous à <br> sauver l'Arctique</p>
+        </div> <!-- .intro__container -->
 
-<div class="intro">
-    
-    <div class="intro__content">
-        <p class="intro__datelimit">J-242</p>
-        <p class="intro__title">Aidez-nous à <br> sauver l'Arctique</p>
-    </div> <!-- .intro__container -->
+         <div class="intro__action">
+            <div class="action__button action__button--sign">
+                <img src="assets/images/icon-sign.svg" >
+                <p class="action__text">Signez</p>
+            </div>
+            <div class="action__button action__button--share">
+                <img src="assets/images/icon-share.svg" >
+                <p class="action__text">Partagez</p>
+            </div>
+        </div> <!-- .intro__action -->
 
-     <div class="intro__action">
-        <div class="action__button action__button--sign">
-            <img src="" >
-            <p class="action__text">Signez</p>
+        <div class="intro__more">
+        	<a href="main-page.php"> En savoir plus </a>
+        	<img src="assets/images/icon-more.svg" >
         </div>
-        <div class="action__button action__button--share">
-            <img src="" >
-            <p class="action__text">Partagez</p>
-        </div>
-    </div> <!-- .intro__action -->
 
-    <div class="intro__more">
-    	<a href="main-page.php"> En savoir plus </a>
-    	<img src="" >
-    </div>
-
-</div> <!-- .intro -->
+    </div> <!-- .intro -->
+</div> <!-- .home -->
 
 <?php include './partials/footer.php';?>
