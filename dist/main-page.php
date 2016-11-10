b4dd079c30d5992787ee150b13b5570d941322ba
Cossid Alexandra (Hetic) <Alexandra@MacBPro-Alex-Cssd.local>
2016-11-10 10:24:39 +0100

    [Main-page] : avancement + omg

--- dist/main-page.php
+++ dist/main-page.php
@@ -7,6 +7,10 @@
             <div class="join__content join__content--share"> Partager </div>
         </div> <!-- .join__container -->
 
+        <div class="timeline__container">
+            
+        </div> <!-- .timeline__container -->
+
         <div class="evolution__container">
             <div class="evolution__svg-container">
                 <svg class="svg__content svg__content--1"> <!-- artique --> </svg>
@@ -20,37 +24,37 @@
                 <div class="animals__liste">
                     <div class="animal__content animal__content--1">
                         <div class="animal__img">
-                            <img src="#" alt="animal-1">
+                            <img src="assets/images/narvals.png" alt="animal-narvals">
                         </div>
                         <div class="animal__svg"></div>
                     </div>
                     <div class="animal__content animal__content--2">
                         <div class="animal__img">
-                            <img src="#" alt="animal-2">
+                            <img src="assets/images/loup.png" alt="animal-loup">
                         </div>
                         <div class="animal__svg"></div>
                     </div>
                     <div class="animal__content animal__content--3">
                         <div class="animal__img">
-                            <img src="#" alt="animal-3">
+                            <img src="assets/images/balaine.png" alt="animal-balaine">
                         </div>
                         <div class="animal__svg"></div>
                     </div>
                     <div class="animal__content animal__content--4">
                         <div class="animal__img">
-                            <img src="#" alt="animal-4">
+                            <img src="assets/images/ours.png" alt="animal-ours">
                         </div>
                         <div class="animal__svg"></div>
                     </div>
                     <div class="animal__content animal__content--5">
                         <div class="animal__img">
-                            <img src="#" alt="animal-5">
+                            <img src="assets/images/seal.png" alt="animal-seal">
                         </div>
                         <div class="animal__svg"></div>
                     </div>
                     <div class="animal__content animal__content--6">
                         <div class="animal__img">
-                            <img src="#" alt="animal-6">
+                            <img src="assets/images/morse.png" alt="animal-morse">
                         </div>
                         <div class="animal__svg"></div>
                     </div>
@@ -62,7 +66,7 @@
         <div class="graph__container">
             <h2 class="container__title"> Une banquise à protéger </h2>    
             <img src="#" alt="graph-perte-75%">
-            <p class="graph__content"> La banquise joue un rôle critique dans la régulation du climat. Mais en raison d changements climatiques, 75% de la surface de la banquise à disparu en 30 ans. </p>
+            <p class="graph__text-container text-container"> La banquise joue un rôle critique dans la régulation du climat. Mais en raison d changements climatiques, 75% de la surface de la banquise à disparu en 30 ans. </p>
         </div> <!-- .graph__container -->
 
         <div class="menaces__container">
@@ -93,19 +97,24 @@
                     <div class="menace__title"> Gazprom </div>
                 </div>
             </div>    
-            <p class="menaces__text-container"> Avec la fonte des glaces, les industriels envisagent d'exploiter le pétrole, les métaux précieux ou les ressources halieutique; et d'emprunter les voies de navigation plus rapide au nord. Les risques sont multiples : marées noires, contamination, polution acoustisque sous-marine,arrivée d'esp§ces invasive, surpêche destruction environnementale... </p>
+            <p class="menaces__text-container text-container"> Avec la fonte des glaces, les industriels envisagent d'exploiter le pétrole, les métaux précieux ou les ressources halieutique; et d'emprunter les voies de navigation plus rapide au nord. Les risques sont multiples : <br> - marées noires <br>- contamination <br>- polution acoustisque sous-marine <br>- arrivée d'espaces invasive <br>- surpêche <br>- destruction environnementale <br>- ... </p>
         </div>
 
         <div class="join-us__container">
-            <div class="join-us__img">
-                <img src="" alt="join-us">
-            </div>
-            <h3 class="join-us__title"> En préservant l'Artique, c'est nous-même que nous préservons</h3>
+            <div class="join-us__img"></div>
+
+            <h3 class="join-us__title"> En préservant l'Arctique, c'est nous-même que nous préservons.</h3>
             <div class="join-us__button"> Rejoingez-nous</div>
             <div class="join-us__social">
-                <a href="javascript();" class="social__facebook"> facebook </a>
-                <a href="javascript();" class="social__twitter"> twitter </a>
-                <a href="javascript();" class="social__google+"> google+ </a>
+                <a href="javascript();">
+                    <img class="social-media" src="assets/images/fb.png" alt="facebook">
+                </a>
+                <a href="javascript();">
+                    <img class="social-media" src="assets/images/twitter.png" alt="twitter">
+                </a>
+                <a href="javascript();">
+                    <img class="social-media" src="assets/images/gplus.png" alt="google+">
+                </a>
             </div>
         </div>
 
