08017ab0a292a282563eff7d0bfcc5573f158953
Cossid Alexandra (Hetic) <Alexandra@macbook-pro-10.home>
2016-11-21 15:02:40 +0100

    [main-page] : svn map

--- app/partials/header.php
+++ app/partials/header.php
@@ -15,8 +15,15 @@
     <script async src="./assets/scripts/modernizr.custom.js"></script>
     <script async src="./assets/scripts/main.js"></script>
     <script async src="./assets/scripts/date.js"></script>
+<<<<<<< Updated upstream
     <script async src="./assets/scripts/classie.js"></script>
     <script async src="./assets/scripts/uiMorphingButton_fixed.js"></script>
+=======
+    <script async src="./assets/scripts/utils.js"></script>
+    <script async src="./assets/scripts/flip-card.js"></script>
+    <script src="./assets/scripts/classie.js"></script>
+    <script src="./assets/scripts/uiMorphingButton_fixed.js"></script>
+>>>>>>> Stashed changes
     <script async src="./assets/scripts/video.js"></script>
   </head>
   <body>
