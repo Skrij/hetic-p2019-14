6cae123634381f4ad47936fdcb1b41143139c91a
LaurenceRouge <laurence.rouge@hetic.net>
2016-11-21 15:19:08 +0100

    [home : main.js] exit automatique du form après submit

--- dist/assets/scripts/main.js
+++ dist/assets/scripts/main.js
@@ -1,6 +1,6 @@
 document.querySelector('.action__button--sign').addEventListener("click", function(){
-	document.querySelector('.form').style.opacity =  '1';
-	document.querySelector('.form').style.zIndex =  '1';
+	document.querySelector('.form').style.opacity =  '10';
+	document.querySelector('.form').style.zIndex =  '10';
 	document.querySelector('.intro').style.opacity =  '0';
 });
 var count = 8192413;
@@ -8,8 +8,13 @@ document.querySelector('.form__submit').addEventListener("click", function(){
 	count++
 	var count_text = count.toString();
 	document.querySelector('.form__count').textContent = count_text;
-	// document.querySelector('.form').style.opacity =  '0';
-	// document.querySelector('.form').style.zIndex =  '0';
+	setTimeout(function(){
+		console.log('close');
+		document.querySelector('.form').style.opacity =  '0';
+		document.querySelector('.form').style.zIndex =  '0';
+		document.querySelector('.intro').style.opacity =  '1';
+		document.querySelector('.intro').style.zIndex =  '1';
+	}, 1000);
 });
 
 document.querySelector('.action__button--share').addEventListener("click", function(){
