0274388c0c5b2cdccafa69f0264e22dfcb1a73ea
LaurenceRouge <laurence.rouge@hetic.net>
2016-11-22 21:31:31 +0100

    [main.js + date.js] clear code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,42 @@
+var form = document.querySelector('.form');
+var intro = document.querySelector('.intro');
+var social = document.querySelector('.social');
+
 document.querySelector('.action__button--sign').addEventListener("click", function(){
-	document.querySelector('.form').style.opacity =  '10';
-	document.querySelector('.form').style.zIndex =  '10';
-	document.querySelector('.intro').style.opacity =  '0';
+    form.style.opacity =  '1';
+    form.style.zIndex =  '10';
+    intro.style.opacity =  '0';
 });
 var count = 8192413;
 document.querySelector('.form__submit').addEventListener("click", function(){
-	count++
-	var count_text = count.toString();
-	document.querySelector('.form__count').textContent = count_text;
-	setTimeout(function(){
-		console.log('close');
-		document.querySelector('.form').style.opacity =  '0';
-		document.querySelector('.form').style.zIndex =  '0';
-		document.querySelector('.intro').style.opacity =  '1';
-		document.querySelector('.intro').style.zIndex =  '1';
-	}, 1000);
+    count++
+    var count_text = count.toString();
+    document.querySelector('.form__count').textContent = count_text;
+    if (document.querySelector('.form__content').checkValidity() || !document.querySelector('.form__content').checkValidity()) 
+    {
+        console.log('INVALID');
+    }
+    else
+    {
+        setInterval(function(){
+            console.log('close');
+            form.style.opacity =  '0';
+            form.style.zIndex =  '0';
+            intro.style.opacity =  '1';
+            intro.style.zIndex =  '1';
+        }, 5000);
+    }
 });
 
 document.querySelector('.action__button--share').addEventListener("click", function(){
-	document.querySelector('.social').style.opacity =  '10';
-	document.querySelector('.social').style.zIndex =  '10';
-	document.querySelector('.intro').style.opacity =  '0';
+    social.style.opacity =  '10';
+    social.style.zIndex =  '10';
+    intro.style.opacity =  '0';
 });
 
 document.querySelector('.social__text').addEventListener("click", function(){
-	document.querySelector('.social').style.opacity =  '0';
-	document.querySelector('.social').style.zIndex =  '0';
-	document.querySelector('.intro').style.opacity =  '1';
+    social.style.opacity =  '0';
+    social.style.zIndex =  '0';
+    intro.style.opacity =  '1';
 });
 
--- dist/assets/scripts/date.js
+++ dist/assets/scripts/date.js
@@ -1,11 +1,11 @@
-currentdate = new Date();
-hour = currentdate.getHours();
-min = currentdate.getMinutes();
-sec = currentdate.getSeconds();
-date_even = new Date(2017, 5, 26, hour, min, sec); //janv =0 > decemb =11
-date1=currentdate.getTime();
-date2=date_even.getTime();
-nbrej= date2-date1;
-nbrej= Math.round((Math.round(nbrej)/1000)/60/60/24-1);
-txtj = nbrej.toString();
+var currentdate = new Date();
+var hour = currentdate.getHours();
+var min = currentdate.getMinutes();
+var sec = currentdate.getSeconds();
+var date_even = new Date(2017, 5, 26, hour, min, sec); //janv =0 > decemb =11
+var date1=currentdate.getTime();
+var date2=date_even.getTime();
+var nbdays= date2-date1;
+nbdays= Math.round((Math.round(nbdays)/1000)/60/60/24-1);
+var txtj = nbdays.toString();
 document.querySelector(".intro__datelimit").textContent = "J -  "+txtj;
